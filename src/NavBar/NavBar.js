62ec3972a13f91c8899d7580ebec5bc4c0eb170b
DK <dhandapanisakthi123@gmail.com>
2023-09-28 23:27:14 +0530

    Third Commit

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Logo from '../assets/logos/DK_LOGO_TRANSPARENT.png';
 import './NavStyles.css';
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
     const [hamburger, setHamburger] = useState(false);
@@ -17,18 +18,10 @@ const NavBar = () => {
                     <img src={Logo} alt='Logo start' width={45} height={45} className='logo' />
                 </div>
                 <div className='menu flex  text-black justify-evenly flex-row gap-16'>
-                    <div onClick={() => {
-                        page('home')
-                    }} className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Home</div>
-                    <div onClick={() => {
-                        page('about')
-                    }} className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>About</div>
-                    <div onClick={() => {
-                        page('projects')
-                    }} className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Projects</div>
-                    <div onClick={() => {
-                        page('contactme')
-                    }} className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Contact Me</div>
+                    <Link to='home' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Home</Link>
+                    <Link to='about' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>About</Link>
+                    <Link to='projects' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Projects</Link>
+                    <Link to='contactme' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Contact Me</Link>
                 </div>
                 <div>
                     <img src={Logo} alt='Logo' width={45} height={45} className='logo end' />
@@ -46,10 +39,10 @@ const NavBar = () => {
             </nav>
             <div className={hamburger ? 'menu-bg view' : 'menu-bg unview'}>
                 <div className={hamburger ? 'menu-card active' : 'menu-card inactive'}>
-                    <div className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Home</div>
-                    <div className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>About</div>
-                    <div className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Projects</div>
-                    <div className='font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Contact Me</div>
+                    <Link to='home' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Home</Link>
+                    <Link to='about' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>About</Link>
+                    <Link to='projects' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Projects</Link>
+                    <Link to='contactme' activeClass='active' offset={-50} duration={500} spy={true} smooth={true} className='links font-Mooli font-bold hover:border-b-2 pb-1 mt-3 hover:text-yellow-500 hover:border-yellow-500 active:border-yellow-500 active:border-b-2 cursor-pointer'>Contact Me</Link>
                 </div>
             </div>
         </>
